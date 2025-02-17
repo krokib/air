d15f5023e7bb60e49836d6e859f24f5b85fecc90
krokib <bblondeau@netcourrier.com>
2025-02-18 00:28:00 +0100

    Modification dans printResult() pour éliminer l'espace intempestif final dans stdout

--- Air08.java
+++ Air08.java
@@ -61,10 +61,10 @@ public class Air08 {
     }
 
     private static void printResult(int[] ints) {
-        for (int i = 0; i < ints.length; i++) {
+        for (int i = 0; i < ints.length - 1; i++) {
             System.out.print(ints[i] + " ");
         }
-        System.out.println("");
+        System.out.println(ints[ints.length - 1]);
     }
 
 
